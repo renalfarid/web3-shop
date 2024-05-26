7a800d2c4aa2139c0ffd8cb9d1de22fc04e643f3
renalfarid <enal.farid@gmail.com>
2024-05-26 12:09:44 +0800

    add alert component

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,11 +1,14 @@
 <script setup>
   import Announcement from "./ui/Announcement.vue"
   import HeaderShop from "./ui/HeaderShop.vue"
+  import Alert from "./ui/Alert.vue"
   import { onMounted, ref } from "vue"
   import { Web3 } from "web3"
 
   const products = ref([])
   const data = ref(null)
+  const isOpen = ref(false)
+  const alertData = ref({})
 
   let web3 = new Web3("https://rpc.sepolia.org");
 
@@ -39,15 +42,13 @@
   const getProduct = async () => {
     await fetchData()
     const contract = new web3.eth.Contract(abiData, contractAddress)
-    console.log("Contract instance:", contract);
-
+   
 
     try {
     const itemCount = await contract.methods.itemCount().call(); // Get the total number of items
       const items = [];
       for (let i = 1; i <= itemCount; i++) {
          const item = await contract.methods.getItem(i).call(); // Get each individual item
-         console.log("item: ", item)
          products.value.push({
           id: Number(item[0]),  // Convert BigInt to number
           name: item[1],
@@ -55,130 +56,57 @@
           price: web3.utils.fromWei(item[3].toString(), 'ether'),  // Convert BigInt to string for fromWei
           owner: item[4]
          })
-         /* products.value.push({
-            id: item[0].toNumber(), // Convert BigNumber to number
-            name: item[1],
-            thumbnail: item[2],
-            price: ethers.utils.formatEther(item[3]), // Convert BigNumber to ether (string)
-            owner: item[4]
-          }); */
       }
-      console.log("All items:", products.value);
       return items;
     } catch (error) {
-      console.error("Error fetching items:", error);
       throw error;
     }
 
   }
 
-
-  
-  /* let provider = ""
-  //await provider.send('eth_requestAccounts', []); // <- this promps user to connect metamask
-  if (typeof window.ethereum !== 'undefined') {
-    provider = new ethers.providers.Web3Provider(window.ethereum);
-    // Proceed with using the provider
-  } else {
-    // Handle the case where MetaMask is not available
-    console.error("MetaMask or Ethereum provider not found.");
-  }
-  
-
-  console.log("provider: ", provider)
-
-  let abiData = null
-
-  const fetchData = async () => {
-    const response = await fetch('/data.json')
-    data.value = await response.json()
-    abiData = data.value.abi
-    //console.log("Data: ", abiData)
-  }
-
-  const getProduct = async () => {
-   await fetchData();
-   const contract = new ethers.Contract(contractAddress, abiData, provider);
-   //const wallet = new ethers.Wallet(contractAddress, abiData)
-   //console.log("contract: ", contract)
-   //const signer = wallet.provider(wallet.address)
-   const signer = provider.getSigner();
-   const contractWithSigner = contract.connect(signer);
-
-   try {
-      const itemCount = await contractWithSigner.itemCount(); // Get the total number of items
-      const items = [];
-      for (let i = 1; i <= itemCount; i++) {
-         const item = await contractWithSigner.getItem(i); // Get each individual item
-         console.log("item: ", item)
-         products.value.push({
-            id: item[0].toNumber(), // Convert BigNumber to number
-            name: item[1],
-            thumbnail: item[2],
-            price: ethers.utils.formatEther(item[3]), // Convert BigNumber to ether (string)
-            owner: item[4]
-          });
-      }
-      console.log("All items:", products.value);
-      return items;
-   } catch (error) {
-      console.error("Error fetching items:", error);
-      throw error;
-   } 
-  }*/
+const sendAlertData = (alertObj) => {
+  alertData.value = alertObj
+}
 
 const buyProduct = async (productId) => {
   await fetchData()
   const contract = new web3.eth.Contract(abiData, contractAddress)
-  console.log("Contract instance:", contract);
-
-  /* const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const contract = new ethers.Contract(contractAddress, abiData, signer);*/
-
+  let alert = {
+    title: "",
+    msg: ""
+  }
   try {
-    // Fetch the product details
     const product = await contract.methods.items(productId).call();
     const price = product.price.toString();
 
-    console.log("Product details:", product);
-    console.log("Sending transaction with price (in Wei):", price);
-
-
-    // Check if the item can be purchased
-    /*if (product.owner === await signer.getAddress()) {
-      alert('You cannot buy your own item.');
-      return;
-    }*/
-
-    // Execute the purchase
     const accounts = await web3.eth.getAccounts();
     if (accounts.length === 0) {
-      console.error("No accounts found. Ensure the wallet is connected.");
       return;
     }
-    console.log("Accounts:", accounts);
     const transaction = await contract.methods.purchaseItem(productId).send({
       from: accounts[0],
       value: price,  // Send value in Wei
       gas: 500000 // Adjust as necessary
     });
 
-    alert('Product purchased successfully');
-  } catch (error) {
-    console.error("Error purchasing item:", error);
+    alert.title = "Product purchased"
+    alert.msg = "Product purchased successfully"
+    sendAlertData(alert)
+    isOpen.value = true
 
-    // Provide detailed error message
-    if (error.data && error.data.message) {
-      alert('Error purchasing item: ' + error.data.message);
-    } else {
-      alert('Error purchasing item: ' + error.message);
-    }
+  } catch (error) {
+    alert.title = "Error purchase"
+    alert.msg = `Error purchasing item:${error.message || error.data.message}`
+    sendAlertData(alert)
+    isOpen.value = true
 
     throw error;
   }
 };
 
+const closeAlert = () => {
+  isOpen.value = false
+}
 onMounted(async () => {
     await initializeWeb3()
     await getProduct()
@@ -188,6 +116,10 @@ onMounted(async () => {
 <template>
   <Announcement />
   <HeaderShop />
+  <div v-if="isOpen">
+    <Alert :data="alertData" @close="closeAlert()" />
+  </div>
+  
   <div class="m-8 px-2 py-2 grid grid-cols-4 gap-4">
     <a v-for="item in products" href="#" class="group block">
     <img
---
+++ src/components/ui/Alert.vue
@@ -0,0 +1,59 @@
+<script setup>
+  import { ref, onMounted } from 'vue'
+
+  const emit = defineEmits(['close'])
+  const props = defineProps(['data'])
+
+  const alerData = ref(null)
+
+  const dismissAlert = () => {
+    emit('close')
+  }
+  onMounted(() => {
+    alerData.value = props.data
+    console.log("alert data", alerData.value)
+  })
+</script>
+<template>
+    <div role="alert" class="rounded-xl border border-gray-100 bg-white p-4">
+      <div class="flex items-start gap-4">
+        <span class="text-green-600">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </span>
+    
+        <div class="flex-1">
+          <strong class="block font-medium text-gray-900"> {{ alerData?.title }} </strong>
+    
+          <p class="mt-1 text-sm text-gray-700 truncate ...">{{ alerData?.msg }}</p>
+        </div>
+    
+        <button @click="dismissAlert()" class="text-gray-500 transition hover:text-gray-600">
+          <span class="sr-only">Dismiss popup</span>
+    
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </template>
